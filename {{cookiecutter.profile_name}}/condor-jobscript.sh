c60a3beddbd37c946d84bf4969ede7223b514db9
Matt Stone <mrstone3@wisc.edu>
2019-02-27 13:57:07 -0600

    add properties to jobscript

--- {{cookiecutter.profile_name}}/condor-jobscript.sh
+++ {{cookiecutter.profile_name}}/condor-jobscript.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#
+# properties = {properties}
+# 
 # condor-jobscript.sh
 #
-# 
+# Wrapper for snakemake jobs
 #
 # Copyright (C) 2019 Matthew Stone <mrstone3@wisc.edu>
 # Distributed under terms of the MIT license.
